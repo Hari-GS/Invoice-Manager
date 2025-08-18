3389d4bef4f12e6a4ec6c78879b85a9831d215da
Hariharan G <sakthihariharan007@gmail.com>
2025-08-18 08:13:34 +0530

    id added in DTO (#8)

--- src/main/java/com/invoiceapp/invoice_API/dto/InvoiceDTO.java
+++ src/main/java/com/invoiceapp/invoice_API/dto/InvoiceDTO.java
@@ -1,13 +1,16 @@
 package com.invoiceapp.invoice_API.dto;
 
 import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
+@Builder
 public class InvoiceDTO {
+    private String id;          // MongoDB id
     private String invoiceId;
     private String vendorName;
     private String totalAmount;
